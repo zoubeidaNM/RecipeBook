e3fc40ea3a21baf2b8ee4f86edfe0cf9c2f23fd6
zoubeidaNM <zoumenz@yahoo.com>
2017-11-02 12:05:58 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/RecipeBookApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBookApplication.class, args);
+	}
+}
